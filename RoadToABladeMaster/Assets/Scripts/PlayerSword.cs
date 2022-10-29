fc65619ef4dc397ddd55c74906b7b861b75e0c21
akibIM <akib1994@gmail.com>
2022-10-29 02:57:42 -0500

    Update: HP / Damage
     - HP per Damage
     - Collider and rigidbody
     - OnTriggerEnter

---
+++ RoadToABladeMaster/Assets/Scripts/PlayerSword.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSword : MonoBehaviour
+{
+    public Health HP_Component;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        other.tag = "Enemy";
+        if (other.tag == HP_Component.tag)
+        {
+            var hpComponent = HP_Component.GetComponent<Health>();
+
+            hpComponent.TakeDamage(1);
+
+            Debug.Log("Works ps");
+        }
+    }
+}
